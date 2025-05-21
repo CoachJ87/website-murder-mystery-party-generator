4e0a8118dd13a6722fb81a5a0781abbbc0f70d97
CoachJ87 <millerdjonathan@gmail.com>
2025-05-21 14:17:34 +0200

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -44,7 +44,7 @@ const CardTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <h3
+  <h4
     ref={ref}
     className={cn(
       "text-lg font-semibold leading-tight line-clamp-2",
